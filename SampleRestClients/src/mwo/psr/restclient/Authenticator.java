be61a6752976001e0d7f587d5d6de259b81fab37
MacApos <maciejapostol98@gmail.com>
2026-05-30 23:32:16 +0200

    subtract

--- SampleRestClients/src/mwo/psr/restclient/Authenticator.java
+++ SampleRestClients/src/mwo/psr/restclient/Authenticator.java
@@ -2,6 +2,8 @@ package mwo.psr.restclient;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.core.MultivaluedMap;
@@ -12,12 +14,14 @@ public class Authenticator implements ClientRequestFilter {
     private final String user;
     private final String password;
 
+
     public Authenticator(String user, String password) {
         this.user = user;
         this.password = password;
     }
 
     public void filter(ClientRequestContext requestContext) throws IOException {
+        float a = (float)Arrays.stream(new int[]{1, 2, 3}).average().orElse(0);
         MultivaluedMap<String, Object> headers = requestContext.getHeaders();
         final String basicAuthentication = getBasicAuthentication();
         headers.add("Authorization", basicAuthentication);
--- ice/src/sr/ice/client/IceClient.java
+++ ice/src/sr/ice/client/IceClient.java
@@ -25,7 +25,8 @@ public class IceClient {
             if(base1 == null) { //powyższa opcja się nie uda, gdy nie był wskazany plik konfiguracyjny (--Ice.Config=client.config)
                 // 2. Uzyskanie referencji obiektu - to samo co powyżej, ale mniej ładnie
                 System.out.println("(using a hard-coded configuration)");
-                base1 = communicator.stringToProxy("calc/calc11:tcp -h 172.29.35.113 -p 10000 -z : udp -h 172.29.35.113 -p 10000 -z"); //opcja -z włącza możliwość kompresji wiadomości
+//                base1 = communicator.stringToProxy("calc/calc11:tcp -h 172.29.35.113 -p 10000 -z : udp -h 172.29.35.113 -p 10000 -z"); //opcja -z włącza możliwość kompresji wiadomości
+                base1 = communicator.stringToProxy("calc/calc11:tcp -h 127.0.0.2 -p 10000 -z : udp -h 127.0.0.2 -p 10000 -z"); //opcja -z włącza możliwość kompresji wiadomości
             }
 
 			// 3. Rzutowanie, zawężanie (do typu Calc)
