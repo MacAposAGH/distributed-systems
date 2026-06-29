706f4c453822cd488fe38dc44ea03977f632bd1b
MacApos <maciejapostol98@gmail.com>
2026-06-29 23:27:30 +0200

    second commit

--- grpc-demo/src/sr/grpc/client/GrpcClient.java
+++ grpc-demo/src/sr/grpc/client/GrpcClient.java
@@ -1,22 +1,35 @@
 package sr.grpc.client;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.protobuf.DoubleValue;
+import com.google.protobuf.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import sr.grpc.gen.GradeBookGrpc;
+import io.grpc.stub.StreamObserver;
+import sr.grpc.gen.*;
+import sr.grpc.gen.GradeBookGrpc.GradeBookFutureStub;
 import sr.grpc.gen.GradeBookGrpc.GradeBookStub;
 
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 public class GrpcClient {
     private final ManagedChannel channel;
-    private final GradeBookStub calcBlockingStub;
+    private final GradeBookStub gradeBookNonblockingStub;
+    private final GradeBookFutureStub gradeBookFutureStub;
+    private final Scanner scanner = new Scanner(System.in);
 
     public GrpcClient(String remoteHost, int remotePort) {
         channel = ManagedChannelBuilder.forAddress(remoteHost, remotePort)
                 .usePlaintext()
                 .build();
-        calcBlockingStub = GradeBookGrpc.newStub(channel);
+        gradeBookNonblockingStub = GradeBookGrpc.newStub(channel);
+        gradeBookFutureStub = GradeBookGrpc.newFutureStub(channel);
     }
 
     public static void main(String[] args) throws Exception {
@@ -24,13 +37,78 @@ public class GrpcClient {
         client.run();
     }
 
+    private String[] extractArgument(String line, List<String> argumentMethods) {
+        String[] split = line.trim().split("\\s*--\\s*");
+        return split.length == 2 && argumentMethods.contains(split[0]) && !split[1].isEmpty() ? split : null;
+    }
+
     private void run() throws InterruptedException {
 
-        Scanner scanner = new Scanner(System.in);
+        final String grades = "grades";
+        final String gpa = "gpa";
+        final String bestSubject = "best-subject";
+        final String bestStudent = "best-student";
+        List<String> requiresArgument = List.of(grades, gpa, bestSubject, bestStudent);
+        String format = "%-10s:%-10s\n";
         while (true) {
             System.out.print("==> ");
-            String line = scanner.nextLine();
-            switch (line) {
+            String method = scanner.nextLine();
+            String argument = null;
+            if (method.contains("--")) {
+                String[] split = extractArgument(method, requiresArgument);
+                if (split == null) {
+                    System.out.println("Invalid argument");
+                    continue;
+                }
+                method = split[0];
+                argument = split[1];
+            }
+
+            switch (method) {
+                case grades:
+                    Student student1 = Student.newBuilder().setName(argument).build();
+                    StreamObserver<Grades> gradesObserver = createResponseObserver((g) -> {
+                        List<Grade> gradesList = g.getGradesList();
+                        System.out.printf("%s's grades:\n", gradesList.getFirst().getStudent().getName());
+                        for (Grade grade : gradesList) {
+                            System.out.printf(format,
+                                    subjectNameToLowerCase(grade.getSubjectName()),
+                                    grade.getGrade());
+                        }
+                    });
+                    gradeBookNonblockingStub.getStudentGrades(student1, gradesObserver);
+                    break;
+                case gpa:
+                    Student student2 = Student.newBuilder().setName(argument).build();
+                    StreamObserver<DoubleValue> gpaObserver = createResponseObserver(d ->
+                            System.out.println(d.getValue()));
+                    gradeBookNonblockingStub.getStudentGpa(student2, gpaObserver);
+                    break;
+                case bestSubject:
+                    Student student3 = Student.newBuilder().setName(argument).build();
+                    StreamObserver<Grade> bestSubjectObserver = createResponseObserver(g ->
+                            System.out.printf(format, subjectNameToLowerCase(g.getSubjectName()), g.getGrade()));
+                    gradeBookNonblockingStub.getStudentBestSubject(student3, bestSubjectObserver);
+                    break;
+                case bestStudent:
+                    SubjectName subjectName = null;
+                    try {
+                        subjectName = SubjectName.valueOf(argument.toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Invalid subject name");
+                    }
+                    Subject subject = Subject.newBuilder().setSubjectName(subjectName).build();
+                    ListenableFuture<Grade> subjectBestStudent = gradeBookFutureStub.getSubjectBestStudent(subject);
+                    FutureCallback<Grade> subjectBestStudentCallback = createFutureCallable(g ->
+                            System.out.printf(format, g.getStudent().getName(), g.getGrade()));
+                    Futures.addCallback(subjectBestStudent, subjectBestStudentCallback, MoreExecutors.directExecutor());
+                    break;
+                case "best-gpa":
+                    ListenableFuture<StudentGpa> bestGpa = gradeBookFutureStub.getBestGpa(Empty.getDefaultInstance());
+                    FutureCallback<StudentGpa> bestGpaCallback = createFutureCallable(s ->
+                            System.out.printf(format, s.getStudent().getName(), s.getGpa().getValue()));
+                    Futures.addCallback(bestGpa, bestGpaCallback, MoreExecutors.directExecutor());
+                    break;
                 case "x":
                 case "":
                     break;
@@ -38,11 +116,10 @@ public class GrpcClient {
                     System.out.println("???");
                     break;
             }
-            if (line.equals("x")) {
+            if (method.equals("x")) {
                 break;
             }
         }
-
         scanner.close();
         shutdown();
     }
@@ -52,4 +129,43 @@ public class GrpcClient {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
+    public String subjectNameToLowerCase(SubjectName subjectName) {
+        return subjectName.toString().toLowerCase();
+    }
+
+    public String subjectNameToLowerCase(Subject subject) {
+        return subjectNameToLowerCase(subject.getSubjectName());
+    }
+
+    public <V> StreamObserver<V> createResponseObserver(Consumer<V> consumer) {
+        return new StreamObserver<>() {
+            @Override
+            public void onNext(V var1) {
+                consumer.accept(var1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("gRPC ERROR");
+            }
+
+            @Override
+            public void onCompleted() {
+            }
+        };
+    }
+
+    public <V> FutureCallback<V> createFutureCallable(Consumer<V> consumer) {
+        return new FutureCallback<V>() {
+            @Override
+            public void onSuccess(V result) {
+                consumer.accept(result);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println("gRPC ERROR");
+            }
+        };
+    }
 }
--- grpc-demo/src/sr/grpc/server/GradeBookImpl.java
+++ grpc-demo/src/sr/grpc/server/GradeBookImpl.java
@@ -1,10 +1,8 @@
 package sr.grpc.server;
 
-import sr.grpc.gen.Grade;
+import com.google.protobuf.DoubleValue;
+import sr.grpc.gen.*;
 import sr.grpc.gen.GradeBookGrpc.GradeBookImplBase;
-import sr.grpc.gen.GradesOpResult;
-import sr.grpc.gen.Student;
-import sr.grpc.gen.SubjectName;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -14,67 +12,64 @@ public class GradeBookImpl extends GradeBookImplBase {
     private final Random random = new Random();
 
     public GradeBookImpl() {
-        createGrades();
+        createData();
     }
 
-    public double randomGrade() {
-        return random.nextInt(0, 7) * 0.5 + 2;
-    }
-
-    public List<Grade> getAllGrades() {
-        return gradeBook.stream()
-                .sorted(Comparator.comparing(Grade::getSubject)
-                        .thenComparing(s -> s.getStudent().getName()))
-                .toList();
-    }
-
-    public List<Grade> getAllGradesByStudentName(String studentName) {
+    public List<Grade> getStudentGrades(String studentName) {
         return gradeBook
                 .stream()
                 .filter(grade -> grade.getStudent().getName().equalsIgnoreCase(studentName))
                 .toList();
     }
 
-    public List<Grade> getAllGradesBySubjectName(SubjectName subjectName) {
+    public List<Grade> getAllSubjectGrades(SubjectName subjectName) {
         return gradeBook
                 .stream()
-                .filter(student -> student.getSubject() == subjectName)
+                .filter(student -> student.getSubjectName() == subjectName)
                 .toList();
     }
 
     public Grade getSubjectBestStudent(SubjectName subjectName) {
-        return getAllGradesBySubjectName(subjectName)
+        return getAllSubjectGrades(subjectName)
                 .stream()
                 .max(Comparator.comparing(Grade::getGrade))
                 .orElse(null);
     }
 
     public Grade getStudentBestSubject(String studentName) {
-        return getAllGradesByStudentName(studentName)
+        return getStudentGrades(studentName)
                 .stream()
                 .max(Comparator.comparing(Grade::getGrade))
                 .orElse(null);
     }
 
-    public double getStudentGpa(String studentName) {
-        return getAllGradesByStudentName(studentName)
+    public DoubleValue getStudentGpa(String studentName) {
+        return DoubleValue.of(getStudentGrades(studentName)
                 .stream()
                 .mapToDouble(Grade::getGrade)
                 .average()
-                .orElse(0);
+                .orElse(0));
     }
 
-    public Map.Entry<Student, Double> getBestGpa() {
+    public StudentGpa getBestGpa() {
         return gradeBook.stream()
                 .collect(Collectors.groupingBy(
                         Grade::getStudent,
                         Collectors.averagingDouble(Grade::getGrade)))
                 .entrySet().stream()
                 .max(Map.Entry.comparingByValue())
+                .map(e -> StudentGpa.newBuilder().setStudent(e.getKey()).setGpa(DoubleValue.of(e.getValue())).build())
                 .orElse(null);
     }
 
-    private void createGrades() {
+
+    public Grades createGrades(List<Grade> grades) {
+        return Grades.newBuilder()
+                .addAllGrades(grades)
+                .build();
+    }
+
+    private void createData() {
         for (String name : List.of("Daniel", "William", "Emma", "Olivia", "Sophia")) {
             Student student = Student.newBuilder()
                     .setName(name)
@@ -84,7 +79,7 @@ public class GradeBookImpl extends GradeBookImplBase {
                     .map(subjectName ->
                             Grade.newBuilder()
                                     .setGrade(randomGrade())
-                                    .setSubject(subjectName)
+                                    .setSubjectName(subjectName)
                                     .setStudent(student)
                                     .build())
                     .toList();
@@ -92,14 +87,51 @@ public class GradeBookImpl extends GradeBookImplBase {
         }
     }
 
+    public double randomGrade() {
+        return random.nextInt(0, 7) * 0.5 + 2;
+    }
+
     @Override
-    public void getAllGradesByStudentName(sr.grpc.gen.StudentNameOpArguments request,
-                                          io.grpc.stub.StreamObserver<sr.grpc.gen.GradesOpResult> responseObserver) {
+    public void getStudentGrades(sr.grpc.gen.Student request,
+                                    io.grpc.stub.StreamObserver<sr.grpc.gen.Grades> responseObserver) {
         String name = request.getName();
-        List<Grade> grades = getAllGradesByStudentName(name);
-        GradesOpResult result = GradesOpResult.newBuilder()
-                .addAllGrades(grades)
-                .build();
+        Grades result = createGrades(getStudentGrades(name));
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getStudentGpa(sr.grpc.gen.Student request,
+                              io.grpc.stub.StreamObserver<com.google.protobuf.DoubleValue> responseObserver) {
+        String name = request.getName();
+        DoubleValue result = getStudentGpa(name);
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getStudentBestSubject(sr.grpc.gen.Student request,
+                                      io.grpc.stub.StreamObserver<sr.grpc.gen.Grade> responseObserver) {
+        String name = request.getName();
+        Grade result = getStudentBestSubject(name);
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getSubjectBestStudent(sr.grpc.gen.Subject request,
+                                      io.grpc.stub.StreamObserver<sr.grpc.gen.Grade> responseObserver) {
+        SubjectName subjectName = request.getSubjectName();
+        Grade result = getSubjectBestStudent(subjectName);
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
+
+
+    @Override
+    public void getBestGpa(com.google.protobuf.Empty request,
+                           io.grpc.stub.StreamObserver<sr.grpc.gen.StudentGpa> responseObserver) {
+        StudentGpa result = getBestGpa();
         responseObserver.onNext(result);
         responseObserver.onCompleted();
     }
